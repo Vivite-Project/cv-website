522e0b7f0e68b66faaec64cc2d5a51d5a94f9369
Axel Virot <vivite@vivitour.home>
2024-09-05 18:19:07 +0200

    [ADD] Added the base of the project

---
+++ src/pages/experiencies.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1>Experiencies</h1>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+const Home: React.FC = () => {
+  const router = useRouter();
+  
+  useEffect(() => {
+    router.replace('/about')
+  }, [router])
+
+  return null;
+}
+
+export default Home;
