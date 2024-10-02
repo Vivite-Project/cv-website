2a2aedd2e9c35652c7b891b5c9b8585d3973a59b
Axel Virot <axel.virot@gmail.com>
2024-10-02 23:31:45 +0200

    [IMPR] Moved the collections folder

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,9 +6,9 @@ import { buildConfig } from "payload";
 import { fileURLToPath } from "url";
 import sharp from "sharp";
 
-import Users from "@collections/Users";
-import Media from "@collections/Media";
-import Pages from "@collections/Pages";
+import Users from "@configs/collections/Users.config";
+import Media from "@configs/collections/Media.config";
+import Pages from "@configs/collections/Pages.config";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
